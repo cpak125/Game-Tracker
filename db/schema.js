f4e69d27c0cabdbea0811b926d8e0bb9fb93a2c8
Chris Pak <chris.pak125@gmail.com>
2018-09-19 16:40:01 -0400

    Completed models

--- db/schema.js
+++ db/schema.js
@@ -1,20 +1,31 @@
 const mongooes = require('mongoose')
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema({
-    username: String,
-    teams: [TeamSchema]
+const GameSchema = new Schema({
+    location: String,
+    opposingTeam: String,
+    date: String,
+    time: String
 })
 
 const TeamSchema = new Schema({
     name: String,
     sport: String,
-    games: []
+    games: [GameSchema]
 })
 
- const GameSchema = new Schema({
-     location: String,
-     opposingTeam: String,
-     date: String,
-     time: String
- })
+const UserSchema = new Schema({
+    username: String,
+    teams: [TeamSchema]
+})
+
+const GameModel = mongoose.model('Game', GameSchema)
+const TeamModel = mongoose.model('Team', TeamSchema)
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = {
+    Game: GameModel,
+    Team: TeamModel,
+    User: UserModel
+}
+
