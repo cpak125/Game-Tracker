9ba72c57f46aac3794bfeed003c4f7485222afd2
Chris Pak <chris.pak125@gmail.com>
2018-09-24 20:05:29 -0400

    Changed title, added more content to home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gametracker' });
+  res.render('index', { title: 'Game Tracker' });
 });
 
 module.exports = router;
